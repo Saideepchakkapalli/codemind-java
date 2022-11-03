269dd05dbc4068d294d54f336b13df3a57fb9247
Saideepchakkapalli <96737408+Saideepchakkapalli@users.noreply.github.com>
2022-11-03 11:09:37 +0530

    import java.util.*; class Solution {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n=sc.nextInt();         int c=0;         for(int i=0;i<n-1;i++)         {             if(i*(i+1)==n)             {                 c=1;             }         }         if(c==1)         {             System.out.println("YES");         }         else         {             System.out.println("NO");         }     } }

---
+++ Pronic_Number_or_Heteromecic_Number.java
@@ -0,0 +1,25 @@
+import java.util.*;
+class Solution 
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int c=0;
+        for(int i=0;i<n-1;i++)
+        {
+            if(i*(i+1)==n)
+            {
+                c=1;
+            }
+        }
+        if(c==1)
+        {
+            System.out.println("YES");
+        }
+        else
+        {
+            System.out.println("NO");
+        }
+    }
+}
